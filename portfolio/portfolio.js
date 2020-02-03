d566faa788f718d1b434c3ac5098ef68194b6252
sabina-rohman <sabina.barbhuiya@yahoo.com>
2020-02-03 20:29:48 +0000

    portfolio

---
+++ portfolio/portfolio.js
@@ -0,0 +1,94 @@
+// Navbar
+$(function () {
+        $(document).scroll(function(){
+          var $nav = $("#mainNavbar");
+          $nav.toggleClass("scrolled",$(this).scrollTop() > $nav.height());
+        });
+      });
+
+// Text
+var TxtRotate = function(el, toRotate, period) {
+  this.toRotate = toRotate;
+  this.el = el;
+  this.loopNum = 0;
+  this.period = parseInt(period, 10) || 2000;
+  this.txt = '';
+  this.tick();
+  this.isDeleting = false;
+};
+
+TxtRotate.prototype.tick = function() {
+  var i = this.loopNum % this.toRotate.length;
+  var fullTxt = this.toRotate[i];
+
+  if (this.isDeleting) {
+    this.txt = fullTxt.substring(0, this.txt.length - 1);
+  } else {
+    this.txt = fullTxt.substring(0, this.txt.length + 1);
+  }
+
+  this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+
+  var that = this;
+  var delta = 300 - Math.random() * 100;
+
+  if (this.isDeleting) { delta /= 2; }
+
+  if (!this.isDeleting && this.txt === fullTxt) {
+    delta = this.period;
+    this.isDeleting = true;
+  } else if (this.isDeleting && this.txt === '') {
+    this.isDeleting = false;
+    this.loopNum++;
+    delta = 500;
+  }
+
+  setTimeout(function() {
+    that.tick();
+  }, delta);
+};
+
+window.onload = function() {
+  var elements = document.getElementsByClassName('txt-rotate');
+  for (var i=0; i<elements.length; i++) {
+    var toRotate = elements[i].getAttribute('data-rotate');
+    var period = elements[i].getAttribute('data-period');
+    if (toRotate) {
+      new TxtRotate(elements[i], JSON.parse(toRotate), period);
+    }
+  }
+  // INJECT CSS
+  var css = document.createElement("style");
+  css.type = "text/css";
+  css.innerHTML = ".txt-rotate > .wrap { border-right: 0.08em solid #666 }";
+  document.body.appendChild(css);
+};
+
+// Back to top button
+    var link = document.getElementById("back-to-top");
+    var amountScrolled = 150;
+
+    function addClass(el, className) {
+
+    if (el.classList) {
+    el.classList.add(className);
+    } else {
+    el.className += ' ' + className;
+    }
+
+    }
+
+    function removeClass(el, className) {
+    if (el.classList)
+    el.classList.remove(className);
+    else
+    el.className = el.className.replace(new RegExp('(^|\\b)' + className.split(' ').join('|') + '(\\b|$)', 'gi'), ' ');
+    }
+
+    window.addEventListener('scroll', function(e) {
+    if ( window.scrollY > amountScrolled ) {
+    addClass(link, 'show');
+    } else {
+    removeClass(link, 'show');
+    }
+    });
